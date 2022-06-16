1919e17644eb3bc021819d278205784654fe5798
Jason Arritt <jsn.arritt@gmail.com>
2022-06-16 15:51:35 -0400

    Route index.js files set up

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// app.use(require("./routes"));
+app.use(require("./routes"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/think-about-it",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
