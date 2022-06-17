aa445f2062cd5a0d750aef646b589872e5bbc859
Jason Arritt <jsn.arritt@gmail.com>
2022-06-17 14:24:54 -0400

    Get all thoughts/POST thought working

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,13 +3,29 @@ const router = require("express").Router();
 const { Thought } = require("../../models");
 
 // GET all thoughts from /api/thoughts
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const thoughts = await Thought.find({});
+    res.json(thoughts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // GET a single thought from /api/thoughts/:id
 router.get("/api/thoughts/:id", async (req, res) => {});
 
 // POST a new thought to /api/thoughts
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+  try {
+    const thought = await Thought.create(req.body);
+    res.json(thought);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // PUT an updated thought into /api/thoughts/:id
 router.put("/:id", async (req, res) => {});
