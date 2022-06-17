c8c73ac557f93fe9a4eed4c781af7feda112ba74
Jason Arritt <jsn.arritt@gmail.com>
2022-06-17 13:12:23 -0400

    Thought and Reaction models created

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,11 +1,38 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
 const reactionSchema = new Schema(
   {
     // Schema
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: function () {
+        return new Types.ObjectId();
+      },
+    },
+    reactionBody: {
+      type: "string",
+      required: true,
+      trim: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    username: {
+      type: "string",
+      required: true,
+    },
+    createdAt: {
+      type: "date",
+      default: Date.now,
+      get: (createdAt) => dateFormat(createdAt),
+    },
   },
   {
     // Options
+    toJSON: {
+      getters: true,
+    },
+    id: false,
   }
 );
 
--- models/Thought.js
+++ models/Thought.js
@@ -1,13 +1,42 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+const reactionSchema = require("./Reaction");
 
 const thoughtSchema = new Schema(
   {
     // Schema
+    thoughtText: {
+      type: "string",
+      required: true,
+      trim: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: "date",
+      default: Date.now,
+      get: (createdAt) => dateFormat(createdAt),
+    },
+    username: {
+      type: "string",
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
   {
     // Options
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
   }
 );
+
+// Virtuals
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
 
 const Thought = model("Thought", thoughtSchema);
 
