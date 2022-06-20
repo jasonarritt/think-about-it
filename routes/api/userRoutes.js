c48802f828bfd00e571388ba21c90d0df9ab8ec5
Jason Arritt <jsn.arritt@gmail.com>
2022-06-20 15:55:01 -0400

    POST friend to user working

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -66,9 +66,45 @@ router.delete("/:id", async (req, res) => {
 });
 
 // POST a new friend to /api/users/:id/friends/:friendId
-router.post("/:id/friends", async (req, res) => {});
+router.post("/:id/friends/", async (req, res) => {
+  try {
+    await User.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $push: { friends: req.body.id } },
+      { new: true, runValidators: true }
+    ).then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with that ID." });
+        return;
+      }
+      res.json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // DELETE a friend from /api/users/:id/friends/:friendId
-router.delete("/:id/friends/:friendId", async (req, res) => {});
+router.delete("/:id/friends/:friendId", async (req, res) => {
+  try {
+    await User.findOneAndUpdate(
+      { _id: req.params.id },
+      { $pull: { friends: { friendId: req.params.friendId } } },
+      { new: true }
+    ).then((dbUserData) => {
+      if (!dbUserData) {
+        res
+          .status(404)
+          .json({ message: "No User or friend found with that ID." });
+        return;
+      }
+      res.json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
